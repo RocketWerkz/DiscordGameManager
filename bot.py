e06695ac380614d0b4915ae5756bf077fea32826
jared.hart <jared.hart@rocketwerkz.com>
2023-09-14 13:24:29 +1200

    + Added API call to Github to set a 'default' branch if update isn't specified.  (helps with first time)

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ import aiohttp
 import filecmp
 import sys
 import time
+import subprocess
+import requests
 
 # Initialize logging
 logger = logging.getLogger('discord')
@@ -26,6 +28,8 @@ BOT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 STAGING_DIRECTORY = os.path.join(BOT_DIRECTORY, 'staging')
 EXTENSIONS_DIRECTORY = os.path.join(BOT_DIRECTORY, 'extensions')
 COGS_DIRECTORY = os.path.join(BOT_DIRECTORY, 'cogs')
+# Random hash included in UNSET_VALUE_ to prevent accidental use of UNSET_VALUE_
+unset = "UNSET_BRANCH_5f3a2b1"
 
 # Create directories if they don't exist
 if not os.path.exists(STAGING_DIRECTORY):
@@ -45,20 +49,47 @@ bot = commands.Bot(command_prefix='*', intents=intents)
 
 
 # Fetch current info as git commit and branch, or commit of a provided branch
-def get_git_info(repo_path, target_branch=None):
+def get_github_default_branch(repo_url):
+    user_repo = "/".join(repo_url.split('/')[-2:])
+    url = f"https://api.github.com/repos/{user_repo}"
     try:
-        if target_branch:
-            # Get the commit object for the specified branch
-            repo = git.Repo(repo_path)
-            branch_commit = repo.refs['refs/remotes/origin/' + target_branch].commit
-            commit = branch_commit.hexsha[:7]  # Just to get the short commit hash
-            branch = target_branch
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()["default_branch"]
+    except Exception as e:
+        logger.error(f"Could not get default branch: {e}")
+        return None
+
+
+def get_git_info(repo_path, target_branch=unset):
+    try:
+        if repo_path.startswith('http') or repo_path.startswith('https'):
+            if target_branch == unset:
+                target_branch = get_github_default_branch(repo_path)
+                if target_branch is None:
+                    raise ValueError("Could not determine the default branch.")
+
+            logger.info(f"Targeting remote repository {repo_path} {target_branch}")
+            cmd = ['git', 'ls-remote', repo_path, f'refs/heads/{target_branch}']
+            result = subprocess.run(cmd, stdout=subprocess.PIPE)
+            output = result.stdout.decode('utf-8').strip()
+
+            if output:
+                commit_hash = output.split()[0]
+                commit = commit_hash[:7]  # Shorten to 7 characters
+                branch = target_branch
         else:
-            # Get the commit object for the current branch
-            repo = git.Repo(BOT_DIRECTORY)
-            repo.git.fetch()
-            commit = repo.head.object.hexsha[:7]  # Just to get the short commit hash
-            branch = repo.active_branch.name
+            repo = git.Repo(repo_path)
+            if target_branch != unset:
+                logger.info(f"Targeting local repository {repo_path} {target_branch}")
+                branch_commit = repo.refs[f"refs/remotes/origin/{target_branch}"].commit
+                commit = branch_commit.hexsha[:7]
+                branch = target_branch
+            else:
+                logger.info(f"Targeting local repository {repo_path} {repo.active_branch.name}")
+                repo.git.fetch()
+                commit = repo.head.object.hexsha[:7]
+                branch = repo.active_branch.name
 
         return commit, branch
 
@@ -67,7 +98,7 @@ def get_git_info(repo_path, target_branch=None):
         raise e
 
     except Exception as e:
-        logger.error(f"Returning None, None: {e} Type: {type(e).__name__}")
+        logger.error(f"Returning exception: {e} Type: {type(e).__name__}")
         raise e
 
 
@@ -76,7 +107,9 @@ def pull_repo(repo_url, target_branch, target_commit):
     try:
         if not os.path.exists(STAGING_DIRECTORY):
             os.makedirs(STAGING_DIRECTORY)
+            logger.info(f"Created directory {STAGING_DIRECTORY}.")
             git.Repo.clone_from(repo_url, STAGING_DIRECTORY)
+            logger.info(f"Cloned repository {repo_url} to {STAGING_DIRECTORY}.")
 
         repo = git.Repo(STAGING_DIRECTORY)
         if repo.is_dirty(untracked_files=True):
@@ -169,12 +202,13 @@ async def update(ctx, *args):
     try:
         # Get git info of production code in BOT_DIRECTORY
         current_commit, current_branch = get_git_info(BOT_DIRECTORY)
+        logger.info(f"Current commit: {current_commit}, current branch: {current_branch}")
 
         # Get git info of GIT_REPO_URL repo
         github_commit, github_branch = get_git_info(GIT_REPO_URL, target_branch=current_branch)
+        logger.info(f"Github commit: {github_commit}, github branch: {github_branch}")
 
-        # Random hash included in UNSET_VALUE_ to prevent accidental use of UNSET_VALUE_
-        unset = "UNSET_BRANCH_5f3a2b1"
+        # Parse arguments
         target_branch = unset
         target_commit = unset
 
@@ -276,7 +310,7 @@ async def on_ready():
 # Command to provide a link to the source code GIT_REPO_URL, state license as AGPL-3.0, strip the .git suffix
 @bot.command(name='source', help='Provide a link to the source code')
 async def source(ctx):
-    await ctx.send(f"Source code available at {GIT_REPO_URL[:-4]} under the Affero GPL-3.0 license.")
+    await ctx.send(f"Source code available at {GIT_REPO_URL} under the Affero GPL-3.0 license.")
 
 
 if __name__ == '__main__':
