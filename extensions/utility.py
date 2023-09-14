d041b6686cabf1a90e615268bee5e6fc03ca7eca
jared.hart <jared.hart@rocketwerkz.com>
2023-09-14 15:36:49 +1200

    * Added error handling and restricted existing commands

--- extensions/utility.py
+++ extensions/utility.py
@@ -3,26 +3,24 @@ from discord.ext import commands
 import platform
 
 
-# Define a Cog for utility commands
 class UtilityCog(commands.Cog):
-    # Initialize the cog
     def __init__(self, bot):
         self.bot = bot
 
-    # Ping command to check latency
-    @commands.command(name='ping')
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('Something went wrong while running the command.')
+
+    @commands.command(name='ping', description='Checks the latency between the bot and the server.')
     async def ping(self, ctx):
-        # Get and send the latency in milliseconds
         await ctx.send(f'Pong! {round(self.bot.latency * 1000)}ms')
 
-    # Info command to display various information
-    @commands.command(name='info')
+    @commands.guild_only()
+    @commands.command(name='info', description='Shows information about the bot.')
     async def info(self, ctx):
-        # Create an embed to display utility information
         embed = discord.Embed(title=f"{ctx.guild.name}", description="Utility Information",
                               timestamp=ctx.message.created_at, color=discord.Color.blue())
-
-        # Add various fields to the embed
         embed.add_field(name="Bot Version", value="1.0")
         embed.add_field(name="Discord.py Version", value=discord.__version__)
         embed.add_field(name="Python Version", value=platform.python_version())
@@ -30,10 +28,11 @@ class UtilityCog(commands.Cog):
         embed.add_field(name="Total Text Channels", value=len(ctx.guild.text_channels))
         embed.add_field(name="Total Voice Channels", value=len(ctx.guild.voice_channels))
 
-        # Send the embed
-        await ctx.send(embed=embed)
+        if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send('I do not have permission to send embeds.')
 
 
-# Function to add the cog to the bot
 async def setup(bot):
     await bot.add_cog(UtilityCog(bot))
